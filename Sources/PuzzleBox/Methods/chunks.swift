6d859fe9f8a637621447656049d4295909c69282
Caleb Wilson <calebwilson706@gmail.com>
2021-05-07 22:11:01 +0100

    chunks of array

---
+++ Sources/PuzzleBox/Methods/chunks.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Caleb Wilson on 07/05/2021.
+//
+
+import Foundation
+
+public extension Array {
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
+    }
+}
