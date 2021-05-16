994fce79c6e04111a73760728821d05dd8c64b2f
Caleb Wilson <calebwilson706@gmail.com>
2021-05-16 22:34:01 +0100

    new method

---
+++ Sources/PuzzleBox/Methods/getMostFrequent.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by Caleb Wilson on 16/05/2021.
+//
+
+import Foundation
+
+func mostFrequent(array: [Int]) -> (value: Int, count: Int)? {
+    var counts = [Int: Int]()
+
+    array.forEach { counts[$0] = (counts[$0] ?? 0) + 1 }
+
+    if let (value, count) = counts.max(by: {$0.1 < $1.1}) {
+        return (value, count)
+    }
+
+    return nil
+}
