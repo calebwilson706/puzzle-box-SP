c031776481344f13dd81f08d854126c87c6d5c15
Caleb Wilson <calebwilson706@gmail.com>
2021-05-20 22:52:55 +0100

    try again

--- Sources/PuzzleBox/DataStructures/DynamicDataStructures/DoublyLinkedList.swift
+++ Sources/PuzzleBox/DataStructures/DynamicDataStructures/DoublyLinkedList.swift
@@ -252,14 +252,14 @@ public extension DoublyLinkedList {
         
         let nthNode = node
         
+        // 0 -> 1 -> 2       3 -> 4 -> 5
+        
         first!.next = last
         
         last!.previous = first
         
-        last = nthNode?.next
+        first = nthNode
         
-        last!.previous = nil
-        
-        nthNode?.next = nil
+        first?.next = nil
     }
 }
