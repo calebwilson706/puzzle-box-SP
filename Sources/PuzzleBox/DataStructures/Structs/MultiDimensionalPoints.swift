1f7fe2fa2899a1a7ce900ad358f792672d04b61d
Caleb Wilson <calebwilson706@gmail.com>
2021-06-01 22:36:17 +0100

    3d and 4d points

---
+++ Sources/PuzzleBox/DataStructures/Structs/MultiDimensionalPoints.swift
@@ -0,0 +1,73 @@
+//
+//  File.swift
+//  
+//
+//  Created by Caleb Wilson on 01/06/2021.
+//
+
+import Foundation
+
+public struct Point3D : Equatable, Hashable {
+    public let x : Int
+    public let y : Int
+    public let z : Int
+    
+    public init(x : Int, y : Int, z : Int){
+        self.x = x
+        self.y = y
+        self.z = z
+    }
+    
+    public var neighbours : [Point3D] {
+        var points = [Point3D]()
+        
+        x.adjacentAndSelf.forEach { x1 in
+            y.adjacentAndSelf.forEach { y1 in
+                z.adjacentAndSelf.forEach { z1 in
+                    points.append(Point3D(x: x1, y: y1, z: z1))
+                }
+            }
+        }
+        
+        points.removeAll { $0 == self }
+        
+        return points
+    }
+    
+}
+
+struct Point4D : Equatable, Hashable {
+    let x : Int
+    let y : Int
+    let z : Int
+    let w : Int
+    
+    public init(x : Int, y : Int, z : Int, w : Int){
+        self.x = x
+        self.y = y
+        self.z = z
+        self.w = w
+    }
+    
+    var neighbours : [Point4D] {
+        let points3D = Point3D(x: x, y: y, z: z).neighbours
+        var results = [Point4D]()
+        
+        points3D.forEach { it in
+            w.adjacentAndSelf.forEach { w1 in
+                results.append(Point4D(x: it.x, y: it.y, z: it.z, w: w1))
+            }
+        }
+        
+        results.append(Point4D(x: x, y: y, z: z, w: w - 1))
+        results.append(Point4D(x: x, y: y, z: z, w: w + 1))
+        
+        return results
+    }
+}
+
+extension Int {
+    var adjacentAndSelf : ClosedRange<Int> {
+        self - 1 ... self + 1
+    }
+}
--- Sources/PuzzleBox/DataStructures/Structs/Point.swift
+++ Sources/PuzzleBox/DataStructures/Structs/Point.swift
@@ -16,28 +16,37 @@ public struct Point : Hashable {
         self.x = x
         self.y = y
     }
+    
     public func up() -> Point {
         return Point(x: x, y: y + 1)
     }
+    
     public func down() -> Point {
         return Point(x: x, y: y - 1)
     }
+    
     public func right() -> Point {
         return Point(x: x + 1, y: y)
     }
+    
     public func left() -> Point {
         return Point(x: x - 1, y: y)
     }
+    
     public func upLeft() -> Point {
         return Point(x: x - 1, y: y + 1)
     }
+    
     public func downLeft() -> Point {
         return Point(x: x - 1, y: y - 1)
     }
+    
     public func upRight() -> Point {
         return Point(x: x + 1, y: y + 1)
     }
+    
     public func downRight() -> Point {
         return Point(x: x + 1, y: y - 1)
     }
+    
 }
